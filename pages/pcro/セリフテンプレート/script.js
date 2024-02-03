59d76a36aa74d315715c810e6da19830a7ee3eed
apln-n <applingo@eis.hokudai.ac.jp>
2024-02-03 16:49:59 +0900

    ある程度できた分をコミット

---
+++ pages/pcro/セリフテンプレート/script.js
@@ -0,0 +1,23 @@
+/*
+    アバター画像の変更
+    参考: https://ryjkmr.com/input-typefile-img-display/ 
+*/
+document.querySelector(".avatar-input > input").addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    const avatarLabel = document.querySelector(".avatar-display > label");
+    const avatarDisplayElement = document.querySelector(".avatar-display > img.display");
+    console.log(file)
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        avatarLabel.textContent = ""
+        avatarDisplayElement.src = reader.result;
+    }, false);
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+});
+
+// セリフの上の名前を自動的に変更する
+document.querySelector(".name > input").addEventListener('input', (e) => {
+    document.querySelector(".line > label").textContent = e.target.value;
+});
